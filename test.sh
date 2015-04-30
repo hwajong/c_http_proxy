a2b55c802597d28c4ef18fe4bf4610c77531b7c0
레오(오화종) <hwajong@coupang.com>
2015-04-30 23:45:33 +0900

    ...

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 #export http_proxy=http://127.0.0.1:47590 && wget http://daum.net/index.html
-export http_proxy=http://127.0.0.1:47590 && wget http://dicman.egloos.com/3050860
+#export http_proxy=http://127.0.0.1:47590 && wget http://dicman.egloos.com/3050860
+export http_proxy=http://127.0.0.1:47590 && wget http://www.httpwatch.com/httpgallery/chunked/chunkedimage.aspx?0.8589904303662479
